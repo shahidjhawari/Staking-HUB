81a77608ca38dc5fa8cf67f1b5475daaf4ab6096
Shahid Iqbal <shahidiqbaljhawari@gmail.com>
2024-08-18 21:41:48 -0700

    latest

--- connection.inc.php
+++ connection.inc.php
@@ -17,6 +17,7 @@ define('SITE_PATH', 'http://localhost/coin/');
 // define('SERVER_PATH', $_SERVER['DOCUMENT_ROOT'] . '/');
 // define('SITE_PATH', 'https://stackinghub.website');
 
+
 define('PRODUCT_IMAGE_SERVER_PATH', SERVER_PATH . 'media/product/');
 define('PRODUCT_IMAGE_SITE_PATH', SITE_PATH . 'media/product/');
 ?>
