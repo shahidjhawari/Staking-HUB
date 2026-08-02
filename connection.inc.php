dfefe7f8115a088c041c4a72ac0cd9f6780f19c3
Shahid Iqbal <shahidiqbaljhawari@gmail.com>
2026-08-02 06:47:59 +0500

    feat: update database connection details and enhance VSCode settings

--- connection.inc.php
+++ connection.inc.php
@@ -19,7 +19,7 @@ if ($localhost) {
         define('SITE_PATH', $protocol . $host . '/');
     } else {
         // Original Online
-        $conn = mysqli_connect("sql213.infinityfree.com", "if0_37082220", "bX5rPVvrqf0SDv", "if0_37082220_coin");
+        $conn = mysqli_connect("sql211.infinityfree.com", "if0_42556896", "HbAi5lwdT0u3i", "if0_42556896_coin");
         $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? "https://" : "http://";
         define('SERVER_PATH', $_SERVER['DOCUMENT_ROOT'] . '/');
         define('SITE_PATH', $protocol . $host . '/');
