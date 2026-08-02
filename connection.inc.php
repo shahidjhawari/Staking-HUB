8e2d919d3e7751ce6e802579de8760d3f1b6cc30
Shahid Iqbal <shahidiqbaljhawari@gmail.com>
2026-08-02 06:40:01 +0500

    feat: update database connection details for online hosting

--- connection.inc.php
+++ connection.inc.php
@@ -13,7 +13,7 @@ if ($localhost) {
     
     if (strpos($host, 'infinityfree.com') !== false) {
         // First Online
-        $conn = mysqli_connect("sql206.infinityfree.com", "if0_36853949", "ZkQAKes45YF34", "if0_36853949_coin");
+        $conn = mysqli_connect("sql211.infinityfree.com", "if0_42556896", "HbAi5lwdT0u3i", "if0_42556896_coin");
         $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? "https://" : "http://";
         define('SERVER_PATH', $_SERVER['DOCUMENT_ROOT'] . '/');
         define('SITE_PATH', $protocol . $host . '/');
